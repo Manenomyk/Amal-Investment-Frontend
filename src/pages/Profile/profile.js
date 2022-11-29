1c62f29e0ab92a3adb7e5dcfe44da194334f9af5
@Bahatimyk <manenomichael99@gmail.com>
2022-11-29 15:58:41 +0300

    profile api

--- src/pages/Profile/profile.js
+++ src/pages/Profile/profile.js
@@ -7,10 +7,77 @@ import Sidebar from '../Sidebar/Sidebar';
 import {Link} from 'react-router-dom';
 import { FaBars } from "react-icons/fa";
 import { IoIosArrowDropleft } from "react-icons/io";
+import { IoIosArrowDropright } from "react-icons/io";
+import axios from 'react'
+
+
+const user = JSON.parse(localStorage.getItem('auth_userName'));
 
 function Profile() {
 
   const [isOpen ,setIsOpen] = useState(false);
+
+  
+  const [ takeInput,  entityInput]= useState({
+    firstName: '',
+    lastName: '',
+    phone_number: '',
+    email: '',
+    citizenship: '',
+    kraPin:'',
+    copyOfID:'',
+    
+
+});
+
+const [firstName, setFirstName] = useState("");
+const [lastName, setLastName ] = useState("")
+const [phone_number, setPhone] = useState("")
+const [email, setEmail] = useState("")
+const [citizenship, setCitizenship] = useState("")
+const [copyOfID, setIdCopy] = useState({})
+const [kraPin, setKra] = useState({})
+
+const pageSubmit = (e) => {
+e.preventDefault();
+const formData = new FormData();
+if(e.target.files && e.target.files[0]){
+formData.append('kraPin', e.target.files[0]);
+}
+formData.append('firstName', firstName);
+formData.append('lastName', lastName);
+formData.append('phone_number', phone_number);
+formData.append('email', email);
+formData.append('citizenship', citizenship);
+formData.append('copyOfID', copyOfID)
+formData.append('kraPin', kraPin)
+
+
+// const data = {
+//   firstName: takeInput.firstName,
+//   lastName: takeInput.lastName,
+//   phone: takeInput.phone,
+//   email: takeInput.email,
+//   citizenship: takeInput.citizenship,
+//   kra: takeInput.kra,
+//   idCopy: takeInput.idCopy,
+
+// }
+axios.post(`/api/user/update-profile/${user.id}`, formData).then (res =>{
+console.log(res);
+if(res.data.status === 200){
+
+localStorage.setItem('auth_token', res.data.token);
+localStorage.setItem('auth_name', res.data.firstName);
+
+}else
+{
+
+}
+
+});
+
+}
 
 
   return (
@@ -51,9 +118,89 @@ function Profile() {
                     <IoMdContact className='contaicon' /> 
                 </div>
                 <div className='d-flex justify-content-center'>
-                    <ProFormi />
-                    
+                  
+                    {/* <ProFormi /> */}
+                    <div>
+                    <p>First name</p>
+                    <input 
+                      className={`form-control shadow-none'}`}
+                      name="firstName"
+                      value={firstName}
+                      autoComplete='off'
+                      id='regInput'
+                      onChange = {(e) => setFirstName(e.target.value)}
+                      />
+
+                      <p>Last Name</p>
+                    <input 
+                      className={`form-control shadow-none'}`}
+                      name="lastName"
+                      value={lastName}
+                      autoComplete='off'
+                      id='regInput'
+                      onChange = {(e) => setLastName(e.target.value)}
+                      />
+
+                      <p>Phone number</p>
+                    <input 
+                      className={`form-control shadow-none'}`}
+                      name="phone_number"
+                      value={phone_number}
+                      autoComplete='off'
+                      id='regInput'
+                      onChange = {(e) => setPhone(e.target.value)}
+                      />
+
+                      <p>Email address</p>
+                    <input 
+                      className={`form-control shadow-none'}`}
+                      name="email"
+                      value={email}
+                      autoComplete='off'
+                      id='regInput'
+                      onChange = {(e) => setEmail(e.target.value)}
+                      />
+
+                      <p>Citizenship</p>
+                      <select name='citizenship' id='citizen' onChange = {(e) => setCitizenship(e.target.value)} className='form-control shadow-none'>
+                        <option value="selected">Citizenship</option>
+                        <option value="">Kenya</option>
+                        <option value="">Uganda</option>
+                        <option value="">Tanzania</option>
+                        <option value="">Rwanda</option>
+                        </select>
+                  </div>
+                  
                 </div>
+                <div className='d-flex mt-3 mb-3 justify-content-center' style={{gap:"1rem"}}>
+                          <input 
+                            className={`form-control shadow-none'}`}
+                            style={{width:"15rem"}}
+                            name="kraPin"
+                            value={kraPin}
+                            autoComplete='off'
+                            id='regInpu'
+                            type="file"
+                            onChange = {(e) => setKra(e.target.files[0])}
+                           
+                            />
+
+                            <input 
+                              className={`form-control shadow-none'}`}
+                              style={{width:"15rem"}}
+                              name="copyOfID"
+                              value={copyOfID}
+                              type="file"
+                              autoComplete='off'
+                              id='regInpu'
+                              onChange = {(e) => setIdCopy(e.target.files[0])}
+                              />
+                        </div>
+
+                    <p className='forgotpass'><b><i>Change password? <IoIosArrowDropright id='arrow' /></i></b></p>
+                    <div className='d-flex justify-content-center'>
+                    <button onClick ={pageSubmit} className='probtn'><i>Update</i></button>
+                    </div>
                 <div>
 
                 </div>
--- src/pages/shares/BuyShares.js
+++ src/pages/shares/BuyShares.js
@@ -5,13 +5,118 @@ import * as buy from 'react-bootstrap';
 import tooltip from '../../images/Assets/icons/tooltip.png';
 import { FaBars } from "react-icons/fa";
 import { IoIosArrowDropleft } from "react-icons/io";
+import axios from 'axios';
+import { Oval } from 'react-loader-spinner';
+import { useNavigate } from 'react-router-dom';
+
+
+
+const user = JSON.parse(localStorage.getItem('auth_userName'));
 
 function BuyShares() {
+
+
+  
   const [isOpen ,setIsOpen] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const [serverError, setServerError] = useState("");
+  const [successResponse,setSuccessResponse]=useState("");
+  const  [ receivedata, setdata] = useState({
+    share:'',
+    amountOfShares:'',
+    agree:'',
+    investAsPerson:'',
+    investAsLegalEntity:''
+
+  });
+
+  const handleinput = (e) =>{
+    e.persist();
+    setdata({...receivedata, [e.target.name]: e.target.value});
+  }
+
+  const detailsubmit = (e) =>{
+    e.preventDefault();
+
+    const data ={
+      share: receivedata.share,
+      amountOfShares: receivedata.amountOfShares,
+      agree: receivedata.agree,
+      investAsPerson: receivedata.investAsPerson,
+      investAsLegalEntity: receivedata.investAsLegalEntity,
+
+    }
+    try {
+      axios.post(`/user/update-share-buy/${user.id}`, data ).then(res =>{
+         console.log(res)
+
+        setLoading(false);
+          if(res.status === 201) {
+
+            setSuccessResponse("you have been registered successfully.");
+            setTimeout(() => {
+              setSuccessResponse("")
+            }, 2000);
+    
+              // alert("registered successfully")
+              navigate('/login');
+  
+          } else {
+  
+          }
+  
+      }) .catch(res =>{
+       
+        
+      setLoading(false);
+      setServerError("Invalid credentials plz check them out")
+      setTimeout(()=>{
+        setServerError("")
+      },2000)
+      
+      });
+      
+
+
+  } catch (error) {
+      
+      // alert("oops, invalid credentials")
+
+      setLoading(false);
+      setServerError("Invalid credentials.")
+      setTimeout(()=>{
+        setServerError("")
+      },2000)
+    
+      navigate('/buyshares');
+  }
+  
+  }
   
   return (
     <div className='d-flex'>
       
+                <div style={{marginLeft:"45%",marginTop:"0%",position:"fixed", zIndex:"2"}}>
+                                {successResponse && (
+                                     <div 
+                                     style={{color:"white",fontSize:"15px",width:"120%",right:"0", background:"#28a745",
+                                     borderRadius: "15px", paddingTop:"15px",paddingBottom:"15px",paddingLeft:"6%",border:"1px solid lightgray",opacity:"0.7",transition:"0.5"}}>
+                                     {successResponse}
+                                    </div>
+                                      
+                                 )}
+                                   {serverError && (
+                                     <div 
+                                    style={{color:"white",fontSize:"15px",width:"120%",right:"0", background:"#ED4337",
+                                    borderRadius: "15px", paddingTop:"15px",paddingBottom:"15px",paddingLeft:"6%",border:"1px solid lightgray",opacity:"0.7",transition:"0.5"}}>
+                                    {serverError}
+                                    </div>
+                                      
+                                 )}
+                          </div>
+
          <div>
                     {!isOpen ?
                   (
@@ -51,9 +156,9 @@ function BuyShares() {
             <buy.Col lg={12}>
               <div id='buyamount' className='d-flex justify-content-center'>
                 <p>How many shares would you like to buy? </p>
-                <input placeholder='12' className='form-control shadow-none' id='buyinp' type="text" />
+                <input placeholder='12' name='shares' onChange={handleinput} value={receivedata.shares} className='form-control shadow-none' id='buyinp' type="text" />
                 <p>Total amount of shares </p>
-                <input placeholder='$24 500' className='form-control shadow-none' id='buyinp1' type="text" />
+                <input placeholder='$24 500' name='amountOfShares' onChange={handleinput} value={receivedata.amountOfShares} className='form-control shadow-none' id='buyinp1' type="text" />
               </div>
             </buy.Col>
           </buy.Row>
@@ -66,27 +171,31 @@ function BuyShares() {
                     <div className="radio-items">
                       <input className="form-check-input"
                         type="radio"
-                        name="flexRadioDefault"
-                        onclick="closeLegal()"
-                        id="flexRadioDefault1" />
-                      <label className="form-check-label" for="flexRadioDefault1">
+                        name="investAsPerson"
+                        onChange={handleinput} value={receivedata.investAsPerson}
+                        // onClick="closeLegal()"
+                        id="flexRadioDefault1" 
+                        />
+                      <label className="form-check-label" htmlFor="flexRadioDefault1">
                         person
                       </label>
                     </div>
                     <div className="radio-items" >
                       <input className="form-check-input"
                         type="radio"
-                        name="flexRadioDefault"
+                        name="investAsLegalEntity"
+                        onChange={handleinput} value={receivedata.investAsLegalEntity}
                         id="flexRadioDefault1"
-                        onclick="openLegal() " />
-                      <label className="form-check-label" for="flexRadioDefault1">
+                        // onClick="openLegal() " 
+                        />
+                      <label className="form-check-label" htmlFor="flexRadioDefault1">
                         Entity
                       </label>
                     </div>
                     <div id="legal-entity" >
 
 
-                <select className='form-control shadow-none'>
+                <select className='form-control shadow-none' name='investAsLegalEntity' onChange={handleinput} value={receivedata.investAsLegalEntity}>
                   <option >Legal entity</option>
                   <option>jamal enterprise</option>
                   <option>Lacharme collection</option>
@@ -101,8 +210,9 @@ function BuyShares() {
                         type="checkbox"
                         name="flexRadioDefault"
                         id="flexRadioDefault1"
-                        onclick="openLegal() " />
-                      <label className="form-check-label" for="flexRadioDefault1">
+                        // onClick="openLegal() " 
+                        />
+                      <label className="form-check-label" htmlFor="flexRadioDefault1" name="agree" onChange={handleinput} value={receivedata.agree}>
                         I agree
                       </label>
                 </div>
@@ -120,7 +230,25 @@ function BuyShares() {
                 />
               </div>
               <div>
-                <button className='buybtn'>BUY</button>
+              <div>
+                        {loading&&(
+                            <button  onClick={detailsubmit} style={{fontSize:"18px",background:"transparent",color:"black",border:"none",marginLeft:"28px"}}><div style={{placeItems:"center",display:"grid",top:"50%",transform:"translate Y(50%)"}}>
+                            <div style={{display:"flex", flexDirection:"row"}}>
+                            <Oval  height="20"
+                            width="20"
+                            color='blue'
+                            ariaLabel='loading'/>
+                        <span style={{fontSize:"20px"}}>Signing in...</span>
+                    </div>
+                </div>
+            </button>
+                        )}
+
+                        {!loading &&(
+                        <button className='buybtn'>BUY</button>
+                        )}
+                    </div>
+                <button onClick={detailsubmit} className='buybtn'>BUY</button>
               </div>
             </buy.Col>
           </buy.Row>
